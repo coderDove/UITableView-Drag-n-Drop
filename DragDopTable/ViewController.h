e369d13a94284cce06b29822718be49e4ad0b1fb
Anton Golub <coder.dove@gmail.com>
2012-03-20 14:07:27 +0200

    Initial Commit

---
+++ DragDopTable/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DragDopTable
+//
+//  Created by Anton Golub on 20.03.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ DragDopTable/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DragDopTable
+//
+//  Created by Anton Golub on 20.03.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
